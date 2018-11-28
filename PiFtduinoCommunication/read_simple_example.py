4b3cfcd3584c9496bfa5a9bdc069e386953024cb
Elena Mokeeva <ryzheboka@gmail.com>
2018-11-27 22:57:58 -1000

    fix port name

--- PiFtduinoCommunication/read_simple_example.py
+++ PiFtduinoCommunication/read_simple_example.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/cu.usbmodem14201', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 time.sleep(10)
 while ser.in_waiting:
     print(ser.readline())
